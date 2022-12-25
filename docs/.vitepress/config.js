e7b1a0a6fbbc1ed20f15701fc7503bbaa79c680a
fangjianchao <fangjianchao@sansi.com>
2022-12-25 21:31:22 +0800

    备份

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,16 @@
 module.exports = {
   title: "Interesting",
   description: "Just playing around.",
+  head: [
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,maximum-scale=1.0,user-scalable=0;",
+      },
+    ],
+    // would render: <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  ],
   markdown: {
     lineNumbers: true,
   },
